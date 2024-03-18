78bef6f8f09fe86b80e5f9b8af17104ac46dc17a
Steven Borrelli <steve@borrelli.org>
2024-03-18 09:29:59 +0100

    add metadata
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- basic/pipeline.sh
+++ basic/pipeline.sh
@@ -2,6 +2,6 @@
 
 DATE=$(date +"%Y-%m-%dT%H:%M:%S%:z")
 cat $1 \
-  | jq '.desired.composite.resource.labels |= {"labelizer.xfn.crossplane.io/crossplane": "rocks"} + .' \
-  | jq --arg date "$DATE" '.desired.composite.resource.annotations |= {"pipeline.crossplane.io/date": $date} + .'
+  | jq '.desired.composite.resource.metadata.labels |= {"labelizer.xfn.crossplane.io/crossplane": "rocks"} + .' \
+  | jq --arg date "$DATE" '.desired.composite.resource.metadata.annotations |= {"pipeline.crossplane.io/date": $date} + .'
 
